8c81fb69642562d0bec6860311a3013834a07efd
RonaldoLara1 <utm23090401@utma.edu.mx>
2025-02-19 10:45:30 -0600

    controllers

--- app/controllers/pokemon.py
+++ app/controllers/pokemon.py
@@ -12,23 +12,12 @@ FP_MODEL = ModelFactory.get_model("pokemon_favorites")
 FP_SCHEMA = FavoriteSchema()
 
 
-@bp.route('/', methods=['POST'])
-def create():
-    try:
-        data = request.json
-        data= FP_SCHEMA.validate(data)
-        fp = FP_MODEL.create(data)
-        return RM.success({"_id": fp})
-    except ValidationError as err:
-        print(err)
-        return RM.error("Es necesario enviar todos los parametros")
-
-@bp.route('/delete/<string:id>', methods=['DELETE'])
-def delete(id):
-    FP_MODEL.delete(ObjectId(id))
-    return RM.success("Pokemon eliminado con exito")
-
 @bp.route('/get_all', methods=['GET'])
-def get_all(user_id):
-    data = FP_MODEL.find_all(user_id)
+def get_all(pokemon_id):
+    data = FP_MODEL.find_all(pokemon_id)
+    return RM.success(data)
+
+@bp.route('/get_one', methods=['GET'])
+def get_by_id(pokemon_id):
+    data = FP_MODEL.find_by_id(pokemon_id)
     return RM.success(data)
