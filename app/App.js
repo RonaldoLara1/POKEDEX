6afb1a8608660e5fbf6128488aeca4a86e7b603a
RonaldoLara1 <utm23090401@utma.edu.mx>
2025-04-06 23:03:56 -0600

    vistas register, edit, password

---
+++ app/App.js
@@ -0,0 +1,76 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      
+      <View>{/*container-image*/}
+        <Image source={{uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/2052px-Pok%C3%A9_Ball_icon.svg.png"}}
+        width={200}
+        height={200}
+        />
+      </View>
+      <View>{/*container-form*/}
+        <Text style={styles.title}>Iniciar Sesion</Text>
+        <Text style={styles.label}>Correo:</Text>
+        <TextInput style={styles.input}/>
+        <Text style={styles.label}>Contraseña:</Text>
+        <TextInput style={styles.input}/>
+        <Pressable style={styles.send}>
+          <Text style={styles.textButton}>Enviar</Text>
+        </Pressable>
+      </View>
+      <View style={styles.containerFooter}>
+        <Text style={styles.containerFooter}>¿Olvidaste tu Contraseña?</Text>
+        <Text style={styles.containerFooter}>Registrate</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10
+  },
+  title:{
+    fontSize:24,
+    fontWeight:"hold"
+  },
+  label:{
+    fontSize:15,
+    fontWeight:'bold'
+  },
+  input:{
+    borderRadius:10,
+    borderWidth:2,
+    borderColor:"black",
+    fontSize:15,
+    width:"auto"
+  },
+  send:{
+    backgroundColor:"red",
+    width:"auto",
+    height:"auto",
+    borderRadius:10,
+    marginTop:15,
+    alignItems:"center",
+    textButton:{
+      color:"white",
+      fontSize:20,
+      fontWeight:"bold",
+    }
+  },
+  containerFooter:{
+    justifyContent:'center',
+    alignItems:"center",
+    texts:{
+      fontSize:20,
+      margin:5
+    }
+  }
+});
